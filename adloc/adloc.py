f6ca5ba11840cc38a6fc487398965323a0277931
zhuwq <zhuwq0@gmail.com>
2024-07-11 15:50:43 -0700

    improve adloc

--- adloc/adloc.py
+++ adloc/adloc.py
@@ -1,56 +1,103 @@
 # %%
-import shelve
 import time
-from pathlib import Path
 
 import numpy as np
 import torch
-from numba import njit
 from torch import nn
 from torch.autograd import Function
 from torch.nn import functional as F
 
-from .eikonal2d import _interp, eikonal_solve, grad_traveltime, traveltime
+# %%
+############################# Not used ########################################
+# from .eikonal2d import _interp
 
 
-class CalcTravelTime(Function):
+# class CalcTravelTime(Function):
+#     @staticmethod
+#     def forward(r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid, zgrid, nr, nz, h):
+#         tt = _interp(timetable, r.numpy(), z.numpy(), rgrid[0], zgrid[0], nr, nz, h)
+#         tt = torch.from_numpy(tt)
+#         return tt
+
+#     @staticmethod
+#     def setup_context(ctx, inputs, output):
+#         r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid, zgrid, nr, nz, h = inputs
+#         ctx.save_for_backward(r, z)
+#         ctx.timetable = timetable
+#         ctx.timetable_grad_r = timetable_grad_r
+#         ctx.timetable_grad_z = timetable_grad_z
+#         ctx.rgrid = rgrid
+#         ctx.zgrid = zgrid
+#         ctx.nr = nr
+#         ctx.nz = nz
+#         ctx.h = h
+
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         timetable_grad_r = ctx.timetable_grad_r
+#         timetable_grad_z = ctx.timetable_grad_z
+#         rgrid = ctx.rgrid
+#         zgrid = ctx.zgrid
+#         nr = ctx.nr
+#         nz = ctx.nz
+#         h = ctx.h
+#         r, z = ctx.saved_tensors
+
+#         grad_r = _interp(timetable_grad_r, r.numpy(), z.numpy(), rgrid[0], zgrid[0], nr, nz, h)
+#         grad_z = _interp(timetable_grad_z, r.numpy(), z.numpy(), rgrid[0], zgrid[0], nr, nz, h)
+
+#         grad_r = torch.from_numpy(grad_r) * grad_output
+#         grad_z = torch.from_numpy(grad_z) * grad_output
+
+#         return grad_r, grad_z, None, None, None, None, None, None, None, None
+
+################################################################################
+
+
+# %%
+class Clamp(torch.autograd.Function):
     @staticmethod
-    def forward(r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid0, zgrid0, nr, nz, h):
-        tt = _interp(timetable, r.numpy(), z.numpy(), rgrid0, zgrid0, nr, nz, h)
-        tt = torch.from_numpy(tt)
-        return tt
-
-    @staticmethod
-    def setup_context(ctx, inputs, output):
-        r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid0, zgrid0, nr, nz, h = inputs
-        ctx.save_for_backward(r, z)
-        ctx.timetable = timetable
-        ctx.timetable_grad_r = timetable_grad_r
-        ctx.timetable_grad_z = timetable_grad_z
-        ctx.rgrid0 = rgrid0
-        ctx.zgrid0 = zgrid0
-        ctx.nr = nr
-        ctx.nz = nz
-        ctx.h = h
+    def forward(ctx, input, min, max):
+        return input.clamp(min=min, max=max)
 
     @staticmethod
     def backward(ctx, grad_output):
-        timetable_grad_r = ctx.timetable_grad_r
-        timetable_grad_z = ctx.timetable_grad_z
-        rgrid0 = ctx.rgrid0
-        zgrid0 = ctx.zgrid0
-        nr = ctx.nr
-        nz = ctx.nz
-        h = ctx.h
-        r, z = ctx.saved_tensors
+        return grad_output, None, None
 
-        grad_r = _interp(timetable_grad_r, r.numpy(), z.numpy(), rgrid0, zgrid0, nr, nz, h)
-        grad_z = _interp(timetable_grad_z, r.numpy(), z.numpy(), rgrid0, zgrid0, nr, nz, h)
 
-        grad_r = torch.from_numpy(grad_r) * grad_output
-        grad_z = torch.from_numpy(grad_z) * grad_output
+def clamp(input, min, max):
+    return Clamp.apply(input, min, max)
 
-        return grad_r, grad_z, None, None, None, None, None, None, None, None
+
+def interp2d(time_table, x, y, xgrid, ygrid, h):
+    nx = len(xgrid)
+    ny = len(ygrid)
+    assert time_table.shape == (nx, ny)
+
+    ix0 = torch.floor((x - xgrid[0]) / h).clamp(0, nx - 2).long()
+    iy0 = torch.floor((y - ygrid[0]) / h).clamp(0, ny - 2).long()
+    ix1 = ix0 + 1
+    iy1 = iy0 + 1
+    # x = (torch.clamp(x, self.xgrid[0], self.xgrid[-1]) - self.xgrid[0]) / self.h
+    # y = (torch.clamp(y, self.ygrid[0], self.ygrid[-1]) - self.ygrid[0]) / self.h
+    x = (clamp(x, xgrid[0], xgrid[-1]) - xgrid[0]) / h
+    y = (clamp(y, ygrid[0], ygrid[-1]) - ygrid[0]) / h
+
+    ## https://en.wikipedia.org/wiki/Bilinear_interpolation
+
+    Q00 = time_table[ix0, iy0]
+    Q01 = time_table[ix0, iy1]
+    Q10 = time_table[ix1, iy0]
+    Q11 = time_table[ix1, iy1]
+
+    t = (
+        Q00 * (ix1 - x) * (iy1 - y)
+        + Q10 * (x - ix0) * (iy1 - y)
+        + Q01 * (ix1 - x) * (y - iy0)
+        + Q11 * (x - ix0) * (y - iy0)
+    )
+
+    return t
 
 
 # %%
@@ -67,6 +114,7 @@ class TravelTime(nn.Module):
         eikonal=None,
         zlim=[0, 30],
         dtype=torch.float32,
+        grad_type="auto",
     ):
         super().__init__()
         self.num_event = num_event
@@ -100,6 +148,17 @@ class TravelTime(nn.Module):
         self.velocity = [velocity["P"], velocity["S"]]
         self.eikonal = eikonal
         self.zlim = zlim
+        self.grad_type = grad_type
+        if self.eikonal is not None:
+            self.timetable_p = torch.tensor(
+                np.reshape(self.eikonal["up"], self.eikonal["nr"], self.eikonal["nz"]), dtype=dtype
+            )
+            self.timetable_s = torch.tensor(
+                np.reshape(self.eikonal["us"], self.eikonal["nr"], self.eikonal["nz"]), dtype=dtype
+            )
+            self.rgrid = self.eikonal["rgrid"]
+            self.zgrid = self.eikonal["zgrid"]
+            self.h = self.eikonal["h"]
 
     def calc_time(self, event_loc, station_loc, phase_type):
         if self.eikonal is None:
@@ -113,16 +172,25 @@ class TravelTime(nn.Module):
             z = event_loc[:, 2] - station_loc[:, 2]
             r = torch.sqrt(x**2 + y**2)
 
-            timetable = self.eikonal["up"] if phase_type == 0 else self.eikonal["us"]
-            timetable_grad = self.eikonal["grad_up"] if phase_type == 0 else self.eikonal["grad_us"]
-            timetable_grad_r = timetable_grad[0]
-            timetable_grad_z = timetable_grad[1]
-            rgrid0 = self.eikonal["rgrid"][0]
-            zgrid0 = self.eikonal["zgrid"][0]
-            nr = self.eikonal["nr"]
-            nz = self.eikonal["nz"]
-            h = self.eikonal["h"]
-            tt = CalcTravelTime.apply(r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid0, zgrid0, nr, nz, h)
+            # timetable = self.eikonal["up"] if phase_type == 0 else self.eikonal["us"]
+            # timetable_grad = self.eikonal["grad_up"] if phase_type == 0 else self.eikonal["grad_us"]
+            # timetable_grad_r = timetable_grad[0]
+            # timetable_grad_z = timetable_grad[1]
+            # rgrid0 = self.eikonal["rgrid"][0]
+            # zgrid0 = self.eikonal["zgrid"][0]
+            # nr = self.eikonal["nr"]
+            # nz = self.eikonal["nz"]
+            # h = self.eikonal["h"]
+            # tt = CalcTravelTime.apply(r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid0, zgrid0, nr, nz, h)
+
+            if phase_type == 0:
+                timetable = self.timetable_p
+            elif phase_type == 1:
+                timetable = self.timetable_s
+            else:
+                raise ValueError("phase_type should be 0 or 1. for P and S, respectively.")
+
+            tt = interp2d(timetable, r, z, self.rgrid, self.zgrid, self.h)
 
             tt = tt.float().unsqueeze(-1)
 
@@ -214,6 +282,16 @@ class TravelTimeDD(nn.Module):
         self.velocity = [velocity["P"], velocity["S"]]
         self.eikonal = eikonal
         self.zlim = zlim
+        if self.eikonal is not None:
+            self.timetable_p = torch.tensor(
+                np.reshape(self.eikonal["up"], self.eikonal["nr"], self.eikonal["nz"]), dtype=dtype
+            )
+            self.timetable_s = torch.tensor(
+                np.reshape(self.eikonal["us"], self.eikonal["nr"], self.eikonal["nz"]), dtype=dtype
+            )
+            self.rgrid = self.eikonal["rgrid"]
+            self.zgrid = self.eikonal["zgrid"]
+            self.h = self.eikonal["h"]
 
     def calc_time(self, event_loc, station_loc, phase_type):
         if self.eikonal is None:
@@ -232,16 +310,26 @@ class TravelTimeDD(nn.Module):
             r = torch.linalg.norm(event_loc[:, :2] - station_loc[:, :2], axis=-1, keepdims=False)  ## nb, 2 (pair), 3
             z = event_loc[:, 2] - station_loc[:, 2]
 
-            timetable = self.eikonal["up"] if phase_type == 0 else self.eikonal["us"]
-            timetable_grad = self.eikonal["grad_up"] if phase_type == 0 else self.eikonal["grad_us"]
-            timetable_grad_r = timetable_grad[0]
-            timetable_grad_z = timetable_grad[1]
-            rgrid0 = self.eikonal["rgrid"][0]
-            zgrid0 = self.eikonal["zgrid"][0]
-            nr = self.eikonal["nr"]
-            nz = self.eikonal["nz"]
-            h = self.eikonal["h"]
-            tt = CalcTravelTime.apply(r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid0, zgrid0, nr, nz, h)
+            # timetable = self.eikonal["up"] if phase_type == 0 else self.eikonal["us"]
+            # timetable_grad = self.eikonal["grad_up"] if phase_type == 0 else self.eikonal["grad_us"]
+            # timetable_grad_r = timetable_grad[0]
+            # timetable_grad_z = timetable_grad[1]
+            # rgrid0 = self.eikonal["rgrid"][0]
+            # zgrid0 = self.eikonal["zgrid"][0]
+            # nr = self.eikonal["nr"]
+            # nz = self.eikonal["nz"]
+            # h = self.eikonal["h"]
+            # tt = CalcTravelTime.apply(r, z, timetable, timetable_grad_r, timetable_grad_z, rgrid0, zgrid0, nr, nz, h)
+
+            if phase_type == 0:
+                timetable = self.timetable_p
+            elif phase_type == 1:
+                timetable = self.timetable_s
+            else:
+                raise ValueError("phase_type should be 0 or 1. for P and S, respectively.")
+
+            tt = interp2d(timetable, r, z, self.rgrid, self.zgrid, self.h)
+
             tt = tt.float().view(nb1, ne1, 1)
 
         return tt
@@ -290,17 +378,68 @@ class TravelTimeDD(nn.Module):
 
 
 class Test(nn.Module):
-    def __init__(self, timetable, rgrid0, zgrid0, nr, nz, h):
+    def __init__(self, timetable, rgrid, zgrid, grad_type="auto", timetable_grad_r=None, timetable_grad_z=None):
         super().__init__()
         self.timetable = timetable
-        self.rgrid0 = rgrid0
-        self.zgrid0 = zgrid0
-        self.nr = nr
-        self.nz = nz
-        self.h = h
+        self.rgrid = rgrid
+        self.zgrid = zgrid
+        self.nr = len(rgrid)
+        self.nz = len(zgrid)
+        self.h = rgrid[1] - rgrid[0]
+        assert self.h == zgrid[1] - zgrid[0]
+        self.grad_type = grad_type
+        self.timetable_grad_r = timetable_grad_r
+        self.timetable_grad_z = timetable_grad_z
+
+    def interp2d(self, time_table, r, z):
+        nr = len(self.rgrid)
+        nz = len(self.zgrid)
+        assert time_table.shape == (nr, nz)
+
+        ir0 = torch.floor((r - self.rgrid[0]) / self.h).clamp(0, nr - 2).long()
+        iz0 = torch.floor((z - self.zgrid[0]) / self.h).clamp(0, nz - 2).long()
+        ir1 = ir0 + 1
+        iz1 = iz0 + 1
+
+        r = (clamp(r, self.rgrid[0], self.rgrid[-1]) - self.rgrid[0]) / self.h
+        z = (clamp(z, self.zgrid[0], self.zgrid[-1]) - self.zgrid[0]) / self.h
+        # r = (torch.clamp(r, self.rgrid[0], self.rgrid[-1]) - self.rgrid[0]) / self.h
+        # z = (torch.clamp(z, self.zgrid[0], self.zgrid[-1]) - self.zgrid[0]) / self.h
+
+        ## https://en.wikipedia.org/wiki/Bilinear_interpolation
+        Q00 = time_table[ir0, iz0]
+        Q01 = time_table[ir0, iz1]
+        Q10 = time_table[ir1, iz0]
+        Q11 = time_table[ir1, iz1]
+
+        t = (
+            Q00 * (ir1 - r) * (iz1 - z)
+            + Q10 * (r - ir0) * (iz1 - z)
+            + Q01 * (ir1 - r) * (z - iz0)
+            + Q11 * (r - ir0) * (z - iz0)
+        )
+
+        return t
 
     def forward(self, r, z):
-        tt = CalcTravelTime.apply(r, z, self.timetable, self.rgrid0, self.zgrid0, self.nr, self.nz, self.h)
+
+        if self.grad_type == "auto":
+            print(self.grad_type)
+            tt = self.interp2d(self.timetable, r, z)
+        else:
+            tt = CalcTravelTime.apply(
+                r,
+                z,
+                self.timetable,
+                self.timetable_grad_r,
+                self.timetable_grad_z,
+                self.rgrid,
+                self.zgrid,
+                self.nr,
+                self.nz,
+                self.h,
+            )
+
         return tt
 
 
@@ -313,23 +452,32 @@ if __name__ == "__main__":
     nr0 = 20
     nz0 = 20
     h = 1
-    r = rgrid0 + h * np.arange(0, nr0)
-    z = zgrid0 + h * np.arange(0, nz0)
-    r, z = np.meshgrid(r, z, indexing="ij")
+    rgrid = rgrid0 + h * np.arange(0, nr0)
+    zgrid = zgrid0 + h * np.arange(0, nz0)
+    r, z = np.meshgrid(rgrid, zgrid, indexing="ij")
     timetable = np.sqrt(r**2 + z**2)
     grad_r, grad_z = np.gradient(timetable, h, edge_order=2)
-    # timetable = torch.from_numpy(timetable.flatten())
-    timetable = timetable.flatten()
+    timetable = torch.from_numpy(timetable)
+    # timetable = timetable.flatten()
+    # grad_r = grad_r.flatten()
+    # grad_z = grad_z.flatten()
 
     nr = 1000
     nz = 1000
-    r = torch.linspace(0, 20, nr)
-    z = torch.linspace(0, 20, nz)
+    r = torch.linspace(-2, 22, nr)
+    z = torch.linspace(-2, 22, nz)
     r, z = torch.meshgrid(r, z, indexing="ij")
     r = r.flatten()
     z = z.flatten()
 
-    test = Test(timetable, rgrid0, zgrid0, nr0, nz0, h)
+    test = Test(
+        timetable,
+        rgrid,
+        zgrid,
+        grad_type="auto",
+        timetable_grad_r=grad_r,
+        timetable_grad_z=grad_z,
+    )
     r.requires_grad = True
     z.requires_grad = True
     tt = test(r, z)
